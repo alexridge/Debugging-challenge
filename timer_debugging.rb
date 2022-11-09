9a1271512aee5e4de10280f56b2c0eb9f51c23c6
Alex <alexridge2309@gmail.com>
2022-11-09 14:55:35 +0000

    debug excersise

---
+++ timer_debugging.rb
@@ -0,0 +1,22 @@
+def encode(plaintext, key)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+  ciphertext_chars = plaintext.chars.map do |char|
+    if 65 + cipher.find_index(char) != 0  
+      (65 + cipher.find_index(char)).chr
+    end   
+  end
+  return ciphertext_chars.join
+end
+
+def decode(ciphertext, key)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+  plaintext_chars = ciphertext.chars.map do |char|
+  cipher[(65 - char.ord)* -1]
+  p cipher[(65 - char.ord)* -1]
+  end
+  print plaintext_chars.join
+end
+
+encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+
