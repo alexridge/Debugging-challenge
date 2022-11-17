352b16eefac5b52a3c2ed17ea76e80d0f0b3634c
Alex <alexridge2309@gmail.com>
2022-11-17 16:25:10 +0000

    finished mock_bite 3

---
+++ testing/spec/test_spec.rb
@@ -0,0 +1,15 @@
+# As a double
+RSpec.describe "example" do
+  it "example" do
+    # This creates the double
+    fake_object = double(:fake_object)
+    # This creates a 'stub' (fake method) on the double
+    # But using 'expect' means the test will fail unless it is called.
+    expect(fake_object).to receive(:greet).with("Kay").and_return("Hello, Kay!")
+
+    # This is just to show you what it can now do
+    expect(fake_object.greet("Kay")).to eq "Hello, Kay!"
+    fake_object.greet("Henry") # This will throw an error
+    # If you comment out the above two lines, the test will fail.
+  end
+end
